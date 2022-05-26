0e2d29add91e7fb39b8ec1f3ac73ec5e0c30c7d9
laura-zabala-tdf <104174255+laura-zabala-tdf@users.noreply.github.com>
2022-05-26 18:09:14 -0300

    Initial commit

---
+++ src/TareaClase12.ts
@@ -0,0 +1,10 @@
+// Una empresa desea premiar a sus empleados con un aumento de sueldo. 
+//Este aumento se ajusta a la siguiente tabla:
+//Sueldo Actual  Sueldo con Aumento
+//0 - 15.000 $                    20%
+//15.001 - 20.000  $          10%
+//20.001 - 25.000  $           5%
+//Más de 25.000 $       No hay aumento
+//Desarrolle un algoritmo dado el salario actual de un empleado determine 
+//el aumento de sueldo a aplicar y se lo muestre
+
